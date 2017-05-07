dd75ca3afffdc4dadc7f3c74a7d7d63974f7b5df
Eyal Posener <posener@gmail.com>
2017-05-07 19:54:42 +0300

    Improve Matcher interface

--- match/match.go
+++ match/match.go
@@ -1,9 +1,11 @@
 package match
 
+import "fmt"
+
 // Matcher matches itself to a string
 // it is used for comparing a given argument to the last typed
 // word, and see if it is a possible auto complete option.
 type Matcher interface {
-	String() string
+	fmt.Stringer
 	Match(prefix string) bool
 }
