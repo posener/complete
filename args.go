ec762b2f87b6813da2f28ed2344657af8e70dc24
Eyal Posener <posener@gmail.com>
2017-05-12 15:19:47 +0300

    Add doc to Args

--- args.go
+++ args.go
@@ -2,9 +2,19 @@ package complete
 
 // Args describes command line arguments
 type Args struct {
+	// All lists of all arguments in command line (not including the command itself)
 	All           []string
+	// Completed lists of all completed arguments in command line,
+	// If the last one is still being typed - no space after it,
+	// it won't appear in this list of arguments.
 	Completed     []string
+	// Last argument in command line, the one being typed, if the last
+	// character in the command line is a space, this argument will be empty,
+	// otherwise this would be the last word.
 	Last          string
+	// LastCompleted is the last argument that was fully typed.
+	// If the last character in the command line is space, this would be the
+	// last word, otherwise, it would be the word before that.
 	LastCompleted string
 }
 
